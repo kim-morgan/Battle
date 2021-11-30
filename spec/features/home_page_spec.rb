5a17c2a247c1c954c11715b9c26c8c08ad449c70
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-11-30 17:49:02 +0000

    Players can enter their names

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,18 @@
 require "capybara/rspec"
 require_relative "../../app"
 Capybara.app = Battle
-feature "serve web app" do
-  scenario "should serve home page" do
+# feature "serve web app" do
+#   scenario "should serve home page" do
+#     visit("/")
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+# end
+feature "fill in player name" do
+  scenario "names should be on screen" do
     visit("/")
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in('name1', with: 'Kim')
+    fill_in('name2', with: 'Chris')
+    click_button('Submit')
+    expect(page).to have_content 'Kim, Chris'
   end
 end
