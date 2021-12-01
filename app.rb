b0ff9bff826bb3d4049dd6165615885accbde2bd
Titus Chang <titus@Tituss-MacBook-Air.local>
2021-12-01 15:21:17 +0000

    Attack button, HP state and attack response added

--- app.rb
+++ app.rb
@@ -1,14 +1,15 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
-
-
 class Battle < Sinatra::Base
   enable :sessions
 
   configure :development do 
     register Sinatra::Reloader
   end
+
+  @@player2_hp = 100
+  @@previous_action = nil
 
   get '/' do
     erb(:index)
@@ -24,6 +25,12 @@ class Battle < Sinatra::Base
     @name1 = session[:name1]
     @name2 = session[:name2]
     erb(:play)
+  end
+
+  post '/attack_player2' do
+    @@player2_hp -= 10
+    @@previous_action = "P2 attacked"
+    redirect '/play'
   end
 
   run! if app_file == $0
