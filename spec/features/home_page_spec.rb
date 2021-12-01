a759c48815eb52e807e6157b014759b5eeada49b
Chris Clement <chrisclement@Chriss-Air.connect>
2021-12-01 12:10:34 +0000

    Add rspec tests for player name fields and welcome title. Add headers to home page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,18 +1,29 @@
-require "capybara/rspec"
-require_relative "../../app"
+require 'capybara/rspec'
+require_relative '../../app'
 Capybara.app = Battle
-# feature "serve web app" do
-#   scenario "should serve home page" do
-#     visit("/")
-#     expect(page).to have_content 'Testing infrastructure working!'
-#   end
-# end
-feature "fill in player name" do
-  scenario "names should be on screen" do
-    visit("/")
+
+feature 'fill in player name' do
+  before(:each) do 
+    visit('/')
+  end
+
+  scenario 'home page welcomes you to battle' do 
+    expect(page).to have_content 'Welcome to Battle!'
+  end
+
+  scenario 'home page has a Player 1 box' do
+    expect(page.has_field?('name1')).to be_truthy
+  end
+
+  scenario 'home page has a Player 2 box' do
+    expect(page.has_field?('name2')).to be_truthy
+  end
+
+  scenario 'names entered should appear on screen after submission' do
     fill_in('name1', with: 'Kim')
     fill_in('name2', with: 'Chris')
     click_button('Submit')
-    expect(page).to have_content 'Kim, Chris'
+    expect(page).to have_content 'Kim vs Chris'
   end
 end
+
